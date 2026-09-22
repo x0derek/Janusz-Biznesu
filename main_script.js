8409f0c58144b48f7b59ea794c1126ecbe5dacf6
Lodomixo <Lodomixo@gmail.com>
2026-09-22 20:33:05 +0200

    Rozpoczęcie pracy nad popupami

---
+++ main_script.js
@@ -0,0 +1,12 @@
+let tileOne = document.getElementById("first-tile")
+tileOne.addEventListener("click", (e) => {
+    console.log("is working!")
+})
+// let tiles = document.getElementsByClassName("grid-item");
+// let popupSpace = document.getElementById("popup-space");
+
+// for (let tile of tiles) {
+//     tile.addEventListener("click", () => {
+//         popupSpace.innerHTML = '<div id="popup" class="grid-item" style="position: fixed; left: 10px; top: 10px; width: 50%; height: 50%; background-color: rgba(0, 0, 0, 0.486); z-index: 999;"></div>'
+//     });
+// }
